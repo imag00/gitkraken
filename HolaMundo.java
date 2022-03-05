c336cf6f7366fdbc13e324878b3cc1bdfcb99046
Ismael Magro <ismael.magro2000@gmail.com>
2022-03-05 16:59:33 +0100

    Metodo sumar

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,10 @@
 public class Cambios {
     public static void main(String[] args) {
         System.out.println("Hola!");
+        sumar(2, 3);
+    }
+
+    private int sumar(int x, int y) {
+        return x + y;
     }
 }
