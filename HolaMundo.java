353e5249bd9bf353c3c938c07e889fedddc1d593
Ismael Magro <ismael.magro2000@gmail.com>
2022-03-05 17:04:58 +0100

    Metodo restar

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,10 @@
 public class Cambios {
     public static void main(String[] args) {
         System.out.println("Hola!");
+        restar(x, y);
+    }
+
+    private int restar(int x, int y) {
+        return x - y;
     }
 }
